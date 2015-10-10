f1eab698063788ccfcb4f0ed77708e7e6d69874c
Anton Demichev <anton.demichev@apsis.com>
2015-10-10 16:48:25 +0200

    seems like it works. run with: elixir script.exs input.txt

--- script.exs
+++ script.exs
@@ -1,9 +1,10 @@
 Enum.at(System.argv, 0) 
   |> File.stream!
-  |> Stream.map(fn {i,a} ->
-    54 - String.codepoints(a) 
+  |> Stream.map(&String.strip/1)
+  |> Stream.filter_map(fn(a) -> String.length(a) > 0 end, fn(a) ->
+    String.codepoints(a) 
       |> Stream.map(&String.to_integer/1) 
       |> Stream.map(&(&1+1))
       |> Enum.sum
-    end) 
-  |> Enum.take(1)
+    end)
+  |> Enum.map(&IO.inspect(54-&1))
