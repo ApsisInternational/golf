cde2027590b0245f2877f07f136182b9ab0f642c
Anton Demichev <anton.demichev@apsis.com>
2015-10-10 16:51:57 +0200

    reduce the script size

--- script.exs
+++ script.exs
@@ -1,10 +1,1 @@
-Enum.at(System.argv, 0) 
-  |> File.stream!
-  |> Stream.map(&String.strip/1)
-  |> Stream.filter_map(fn(a) -> String.length(a) > 0 end, fn(a) ->
-    String.codepoints(a) 
-      |> Stream.map(&String.to_integer/1) 
-      |> Stream.map(&(&1+1))
-      |> Enum.sum
-    end)
-  |> Enum.map(&IO.inspect(54-&1))
+Enum.at(System.argv,0)|>File.stream!|>Stream.map(&String.strip/1)|>Stream.filter_map(fn(a)->a<>""end,fn(a)->String.codepoints(a)|>Stream.map(&String.to_integer/1)|>Stream.map(&(&1+1))|>Enum.sum end)|>Enum.map(&IO.inspect(54-&1))
